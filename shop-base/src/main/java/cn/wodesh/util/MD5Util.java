b9db8bd4b9d22b7e52203b77ac475f47efa693d8
tanshuai <Pass2word>
2018-04-20 17:11:26 +0800

    MD5修改

--- shop-base/src/main/java/cn/wodesh/util/MD5Util.java
+++ shop-base/src/main/java/cn/wodesh/util/MD5Util.java
@@ -1,65 +1,32 @@
 package cn.wodesh.util;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MD5Util {
 
-	private static String byteArrayToHexString(byte b[]) {
-		StringBuffer resultSb = new StringBuffer();
-		for (int i = 0; i < b.length; i++)
-			resultSb.append(byteToHexString(b[i]));
-
-		return resultSb.toString();
-	}
-
-	private static String byteToHexString(byte b) {
-		int n = b;
-		if (n < 0)
-			n += 256;
-		int d1 = n / 16;
-		int d2 = n % 16;
-		return hexDigits[d1] + hexDigits[d2];
-	}
-
-	public static String MD5Encode(String origin, String charsetname) {
-		String resultString = null;
+	public static String MD5(String s) {
 		try {
-			resultString = new String(origin);
 			MessageDigest md = MessageDigest.getInstance("MD5");
-			if (charsetname == null || "".equals(charsetname))
-				resultString = byteArrayToHexString(md.digest(resultString
-						.getBytes()));
-			else
-				resultString = byteArrayToHexString(md.digest(resultString
-						.getBytes(charsetname)));
-		} catch (Exception e) {
-			e.printStackTrace();
+			byte[] bytes = md.digest(s.getBytes("utf-8"));
+			return toHex(bytes);
 		}
-		return resultString;
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-	private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5",
-			"6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
-	
-	
-    public static String getPwd(String pwd) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("md5");
-            byte[] bs = digest.digest(pwd.getBytes());
-            String hexString = "";
-            for (byte b : bs) {
-                int temp = b & 255;
-                if (temp < 16 && temp >= 0) {
-                    hexString = hexString + "0" + Integer.toHexString(temp);
-                } else {
-                    hexString = hexString + Integer.toHexString(temp);
-                }
-            }
-            return hexString;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
+	private static String toHex(byte[] bytes) {
+
+		final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
+		StringBuilder ret = new StringBuilder(bytes.length * 2);
+		for (int i=0; i<bytes.length; i++) {
+			ret.append(HEX_DIGITS[(bytes[i] >> 4) & 0x0f]);
+			ret.append(HEX_DIGITS[bytes[i] & 0x0f]);
+		}
+		return ret.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(MD5("Sobeyhive-2016").toLowerCase());
+	}
 
 }
