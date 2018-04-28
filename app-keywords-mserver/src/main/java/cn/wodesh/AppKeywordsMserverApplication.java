44dfac8234b597d74fd5caddae852e6210dc1013
tanshuai <tanshuai@sobey.com>
2018-04-28 21:48:07 +0800

    新建关键字

---
+++ app-keywords-mserver/src/main/java/cn/wodesh/AppKeywordsMserverApplication.java
@@ -0,0 +1,12 @@
+package cn.wodesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppKeywordsMserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppKeywordsMserverApplication.class, args);
+	}
+}
